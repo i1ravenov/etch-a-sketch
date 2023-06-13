9f63dbd3ed04fbed8f4b850756f3d005dde1ec6a
i1ravenov <i1ravenov@gmail.com>
2023-06-13 15:14:33 +0300

    Add styles, js script refactoring

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-
-
+const value = document.querySelector("#value");
 const sizeSlider = document.querySelector("#size-slider");
 console.log(sizeSlider);
-const d = +sizeSlider.value;
-const container = document.querySelector(".container");
-console.log(container);
-const CONTAINER_WIDTH = document.querySelector(".container").offsetWidth;
+value.textContent = sizeSlider.value;
 
-console.log(CONTAINER_WIDTH);
-const BLOCK_WIDTH = Math.floor(CONTAINER_WIDTH / d);
+setGrid(sizeSlider.value);
+sizeSlider.addEventListener("input", (e) => {
+	let d = +e.target.value;
+	value.textContent = d;
+	setGrid(d);
+});
 
-for (let i = 0; i < d; i++) {
-	for (let j = 0; j < d; j++) {
-		const div = document.createElement('div');
-		div.classList.add('block');
-		div.style.width = BLOCK_WIDTH + "px";
-		div.style.height = BLOCK_WIDTH + "px";
-		container.appendChild(div);
+
+function setGrid(dim) {
+	const CONTAINER_WIDTH = document.querySelector(".container").clientHeight;
+	console.log(CONTAINER_WIDTH);
+	const BLOCK_WIDTH = Math.floor(CONTAINER_WIDTH / dim);
+	const container = document.querySelector(".container");
+	container.innerHTML = "";
+	for (let i = 0; i < dim; i++) {
+		for (let j = 0; j < dim; j++) {
+			const div = document.createElement('div');
+			div.classList.add('block');
+			div.style.width = BLOCK_WIDTH + "px";
+			div.style.height = BLOCK_WIDTH + "px";
+			div.addEventListener('mouseover', (e) => {
+				e.target.style['background-color'] = 'black';
+			});
+			container.appendChild(div);
+		}
 	}
 }
