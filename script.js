24c345ef7bfcfc72cdde5d36997b482f1408d7e4
i1ravenov <i1ravenov@gmail.com>
2023-06-15 14:46:38 +0300

    Add rainbow buttons and other improvements

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const value = document.querySelector("#value");
-const sizeSlider = document.querySelector("#size-slider");
-console.log(sizeSlider);
+function $(selector) {
+	return document.querySelector(selector);
+}
+
+const value = $("#value");
+const sizeSlider = $("#size-slider");
 value.textContent = sizeSlider.value;
+const DEFAULT_COLOR = 'black';
+const BG_COLOR = 'aqua';
+let color = DEFAULT_COLOR;
+
+const resetButton = $("#reset");
+const eraseButton = $("#erase");
+const chooseButton = $("#choose");
+const colorInput = $('#picker');
+const rainbow = $("#rainbow");
+
+colorInput.addEventListener('change', (e) => { color = e.target.value });
+resetButton.addEventListener('click', (e) => setGrid(sizeSlider.value));
+eraseButton.addEventListener('click', (e) => { color = BG_COLOR });
+chooseButton.addEventListener('click', (e) => { color = colorInput.value });
+rainbow.addEventListener('click', (e) => color = 'rainbow');
 
 setGrid(sizeSlider.value);
 sizeSlider.addEventListener("input", (e) => {
@@ -12,10 +30,9 @@ sizeSlider.addEventListener("input", (e) => {
 
 
 function setGrid(dim) {
-	const CONTAINER_WIDTH = document.querySelector(".container").clientHeight;
-	console.log(CONTAINER_WIDTH);
+	const CONTAINER_WIDTH = $(".container").clientHeight;
 	const BLOCK_WIDTH = Math.floor(CONTAINER_WIDTH / dim);
-	const container = document.querySelector(".container");
+	const container = $(".container");
 	container.innerHTML = "";
 	for (let i = 0; i < dim; i++) {
 		for (let j = 0; j < dim; j++) {
@@ -24,9 +41,24 @@ function setGrid(dim) {
 			div.style.width = BLOCK_WIDTH + "px";
 			div.style.height = BLOCK_WIDTH + "px";
 			div.addEventListener('mouseover', (e) => {
-				e.target.style['background-color'] = 'black';
+				if (color === 'rainbow') {
+					e.target.style['background-color'] = randomColor();
+				} else {
+					e.target.style['background-color'] = color;
+				}
 			});
 			container.appendChild(div);
 		}
 	}
+}
+
+function randomColor() {
+	const r = randomHue();
+	const g = randomHue();
+	const b = randomHue();
+	return `rgb(${r},${g},${b})`;
+}
+
+function randomHue() {
+	return Math.floor(Math.random() * 256);
 }
